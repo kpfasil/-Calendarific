ebfc0fa4ef1eef0fdd76dd419ed80755618c3e7b
kp fasil <fasilkpsm@gmail.com>
2025-02-13 11:11:14 +0530

    intial commit - calender pro

---
+++ calender_be/holidays/views.py
@@ -0,0 +1,28 @@
+import requests
+from django.core.cache import cache
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from django.conf import settings
+
+@api_view(['GET'])
+def holidays(request):
+    country = request.GET.get('country')
+    year = request.GET.get('year')
+    cache_key = f'holidays_{country}_{year}'
+
+    # Check if data is cached
+    cached_data = cache.get(cache_key)
+    if cached_data:
+        return Response(cached_data)
+
+    # Fetch data from Calendarific API
+    url = f'https://calendarific.com/api/v2/holidays?api_key={settings.CALENDARIIC_API_KEY}&country={country}&year={year}'
+    print(url)
+    response = requests.get(url)
+    data = response.json()
+
+    # Cache the data for 24 hours
+    cache.set(cache_key, data, timeout=86400)
+
+    return Response(data)
